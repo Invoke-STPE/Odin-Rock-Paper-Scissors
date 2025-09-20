17b603e790720bd40ce419cf3f0d62c36fe58ef2
Steven <47424622+Invoke-STPE@users.noreply.github.com>
2025-09-20 20:02:36 +0200

    Implement getComputerChoice

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Hello world");
+const rock = 0;
+const paper = 1;
+const scissors = 2;
+
+const choices = {
+    [rock]: "rock",
+    [paper]: "paper",
+    [scissors]: "scissors"
+}
+
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+
+    return choices[randomNumber];
+}
